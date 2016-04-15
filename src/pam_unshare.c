3ceeda16e6166ae448a639b0ebdff72b8d0b62b3
Giles Thomas <giles@pythonanywhere.com>
2016-04-15 14:31:30 +0000

    Stole syslog function from code.google.com/p/pam-chroot

--- src/pam_unshare.c
+++ src/pam_unshare.c
@@ -1,16 +1,30 @@
+#include <syslog.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 #define  PAM_SM_SESSION
 #include <security/pam_modules.h>
 
+
+static void _pam_log(int err, const char *format, ...) {
+  va_list args;
+
+  va_start(args, format);
+  openlog("pam_unshare", LOG_PID, LOG_AUTHPRIV);
+  vsyslog(err, format, args);
+  va_end(args);
+  closelog();
+}
+
+
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    printf("pam_unshare pam_sm_open_session\n");
+    _pam_log(LOG_DEBUG, "pam_unshare pam_sm_open_session");
     return PAM_SUCCESS;
 }
 
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-    printf("pam_unshare pam_sm_close_session\n");
+    _pam_log(LOG_DEBUG, "pam_unshare pam_sm_close_session");
     return PAM_SUCCESS;
 }
