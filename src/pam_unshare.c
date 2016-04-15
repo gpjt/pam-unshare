c39b44c7f802b1cfcf367a38336a078738b33a9b
Giles Thomas <giles@pythonanywhere.com>
2016-04-15 14:17:59 +0000

    Further changes to match the official guide at http://www.linux-pam.org/Linux-PAM-html/mwg-expected-of-module-session.html

--- src/pam_unshare.c
+++ src/pam_unshare.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <security/pam_appl.h>
+#define  PAM_SM_SESSION
 #include <security/pam_modules.h>
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
