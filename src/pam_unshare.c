0b3b2c730aa260f1d060f5f70cf871174da28fc8
Giles Thomas <giles@pythonanywhere.com>
2016-04-15 14:07:19 +0000

    Make it print something on session open/close

--- src/pam_unshare.c
+++ src/pam_unshare.c
@@ -5,10 +5,12 @@
 #include <security/pam_modules.h>
 
 PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    printf("pam_unshare pam_sm_open_session\n");
     return PAM_SUCCESS;
 }
 
 
 PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+    printf("pam_unshare pam_sm_close_session\n");
     return PAM_SUCCESS;
 }
